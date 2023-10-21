1efaaa453194ad2218065d7860d3e2b7b05220aa
ffreyer <frederic481994@hotmail.de>
2023-10-21 14:08:59 +0200

    dependencies and other init

---
+++ build.bat
@@ -0,0 +1,51 @@
+:: Note that this expects GLFW to be compiled already
+
+@ECHO ON
+SetLocal EnableDelayedExpansion
+
+ECHO "Building ${PWD}"
+
+mkdir bin
+
+:: Get a list of all the .c and .cpp files.
+cd src
+SET cFilenames= 
+:: "dependencies/glad/src/gl.c"
+FOR /R %%f in (*.cpp) do (
+    SET cFilenames=!cFilenames! "%%f"
+)
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! "%%f"
+)
+cd ..
+
+:: ECHO "Files:" $cFilenames
+
+SET assembly=main
+SET compilerFlags=-g 
+REM -Wall -Werror
+SET includeFlags=-Isrc -Idependencies/entt/src/entt -Idependencies/glad/include -Idependencies/glfw/include -Idependencies/glm -Idependencies/stb
+SET linkerDirs=-Ldependencies/glfw/lib
+SET linkerFlags=-lglfw3dll -lglew32 -lopengl32
+REM SET defines=
+
+ECHO "Building $assembly..."
+:: TODO: check if this works w/o full path
+"C:\\Program Files\\LLVM\\bin\\clang++.exe" %cFilenames%  %compilerFlags% -o bin/%assembly%.exe -std=c++20 %includeFlags% %linkerDirs% %linkerFlags%
+REM %defines% 
+
+
+ECHO "All assemblies built successfully."
+
+:: OK, let's try to actually understand this
+:: up to the for loop everything's easy enough
+::
+:: FOR /R %%f in (*.cpp) do (
+::     SET cFilenames=!cFilenames! "%%f"
+:: )
+::
+:: %%f is the variable (full path)
+:: %% makes the loop run in file, % in cmd prompt
+:: (*.cpp) is the set, regex matching cpp files I suppose
+:: /R runs the loop in every recursive directory
+:: !cFilenames! expands at run time rather than parse time like %cFilenames% would

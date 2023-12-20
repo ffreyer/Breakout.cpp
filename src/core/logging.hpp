f29e95887a8078adce5a5b930f30b85c79241f66
ffreyer <frederic481994@hotmail.de>
2023-12-20 13:21:35 +0100

    fix unrecognized character warnings

--- src/core/logging.hpp
+++ src/core/logging.hpp
@@ -4,31 +4,31 @@
 
 // colored printing for very basic logging
 namespace LOG {
-    static const char* reset = "\e[0m";
+    static const char* reset = "\33[0m";
 
-    static const char* black = "\e[0;30m";
-    static const char* BLACK = "\e[1;30m";
+    static const char* black = "\33[0;30m";
+    static const char* BLACK = "\33[1;30m";
 
-    static const char* red = "\e[0;31m";
-    static const char* RED = "\e[1;31m";
+    static const char* red = "\33[0;31m";
+    static const char* RED = "\33[1;31m";
 
-    static const char* green = "\e[0;32m";
-    static const char* GREEN = "\e[1;32m";
+    static const char* green = "\33[0;32m";
+    static const char* GREEN = "\33[1;32m";
 
-    static const char* yellow = "\e[0;33m";
-    static const char* YELLOW = "\e[1;33m";
+    static const char* yellow = "\33[0;33m";
+    static const char* YELLOW = "\33[1;33m";
 
-    static const char* blue = "\e[0;34m";
-    static const char* BLUE = "\e[1;34m";
+    static const char* blue = "\33[0;34m";
+    static const char* BLUE = "\33[1;34m";
 
-    static const char* magenta = "\e[0;35m";
-    static const char* MAGENTA = "\e[1;35m";
+    static const char* magenta = "\33[0;35m";
+    static const char* MAGENTA = "\33[1;35m";
 
-    static const char* cyan = "\e[0;36m";
-    static const char* CYAN = "\e[1;36m";
+    static const char* cyan = "\33[0;36m";
+    static const char* CYAN = "\33[1;36m";
 
-    static const char* white = "\e[0;37m";
-    static const char* WHITE = "\e[1;37m";
+    static const char* white = "\33[0;37m";
+    static const char* WHITE = "\33[1;37m";
 
     [[maybe_unused]] static std::ostream& __log(const char* color) {
         return (std::cout << color);
@@ -40,4 +40,3 @@ namespace LOG {
     #define ERROR LOG::__log(LOG::red)
     #define ENDLOG LOG::_log(LOG::reset) << std::endl
 }
-
